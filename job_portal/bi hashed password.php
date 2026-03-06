ffc529e542b4e15a7344b689ea8fba76e521916a
Rooba8925 <168180602+Rooba8925@users.noreply.github.com>
2026-03-06 22:25:32 +0530

    Delete job_portal directory

--- job_portal/bi hashed password.php
+++
@@ -1,4 +0,0 @@
-$password = 'roobs@123';
-$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-
-echo $hashedPassword;
--- job_portal/hash_generator.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo password_hash("5050", PASSWORD_BCRYPT);
-?>
